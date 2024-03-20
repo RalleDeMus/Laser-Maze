166ff718a43581132fa42d2175de9e6fb76dc862
s204427 <s204427@student.dtu.dk>
2024-03-20 12:04:04 +0100

    Added basic board class elements

--- src/Board.java
+++ src/Board.java
@@ -3,23 +3,43 @@ public class Board {
     // Tiles array
     // Cursor tile
 
-    // Laser tree
+    int boardSize;
+    Tile[][] tiles;
+
+    Tile cursorTile;
+
+    // Laser tree ???
+
 
     // Constructor (json file)
         // Gets the board size
         // Initializes the tiles array based json file
+        // Initializes the cursor tile
+    public Board(int boardSize, int squareSize, assetServer) {
+        this.boardSize = boardSize;
+        this.tiles = new Tile[boardSize][boardSize];
+        this.cursorTile = new Tile();
+    }
 
     // Draw the board
         // Draw the tiles: Empty or with a mirror
         // Draw the selected tile
+        // Draw the cursor tile
+
 
     // Construct laser tree
 
     // Check if win condition is met after laser tree
 
     // Add the cursor tile to the board and check if placement is valid
+    public void addTile(int x, int y) {
+        tiles[x,y] = cursorTile;
+    }
 
     // Remove a tile
+    public void removeTile(int x, int y) {
+        tiles[x,y] = null; //empty tile?
+    }
 
     // Rotate the cursor tile
 }
