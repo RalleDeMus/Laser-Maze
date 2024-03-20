17f47f5dda38d77d6d7a74a993f1388cb7c7f7b7
s204427 <s204427@student.dtu.dk>
2024-03-20 11:49:51 +0100

    Added board class comments

---
+++ src/Board.java
@@ -0,0 +1,27 @@
+public class Board {
+    // Board size
+    // Tiles array
+    // Cursor tile
+
+    // Laser tree
+
+    // Constructor (json file)
+        // Gets the board size
+        // Initializes the tiles array based json file
+
+    // Draw the board
+        // Draw the tiles: Empty or with a mirror
+        // Draw the selected tile
+
+    // Construct laser tree
+
+    // Check if win condition is met after laser tree
+
+    // Add the cursor tile to the board and check if placement is valid
+
+    // Remove a tile
+
+    // Rotate the cursor tile
+}
+
+
