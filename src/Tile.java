c4f4de874100f0442885d0368906dfeb684c4cc4
RalleDeMus <Rasmus-Harbo@hotmail.com>
2024-03-20 11:58:36 +0100

    Tile interface and class

---
+++ src/Tile.java
@@ -0,0 +1,56 @@
+import java.awt.image.BufferedImage;
+
+public class Tile implements TileInterface{
+    private boolean isMoveable;
+
+    private boolean isRotateable;
+    private BufferedImage image;
+
+    // getter and setter of if the tile is moveable or not
+    @Override
+    public boolean getIsMoveable() {
+        return isMoveable;
+    }
+
+    @Override
+    public void setIsMoveable(boolean isMoveable) {
+        this.isMoveable = isMoveable;
+
+    }
+    // getter and setter of image for tile
+    @Override
+    public void setImage(BufferedImage image) {
+        this.image = image;
+    }
+
+    @Override
+    public BufferedImage getImage() {
+        return image;
+    }
+
+    //getter and setter to make tile rotateable
+    @Override
+    public boolean getIsRotateable() {
+        return isRotateable;
+    }
+
+    @Override
+    public void setIsRotateable(boolean isRotateable) {
+        this.isRotateable = isRotateable;
+    }
+
+    @Override
+    public void placeTile() {
+
+    }
+
+    @Override
+    public void removeTile() {
+
+    }
+}
+/*
+void getOrientation();
+void setOrientation();
+
+ */
---
+++ src/TileInterface.java
@@ -0,0 +1,13 @@
+import java.awt.image.BufferedImage;
+
+public interface TileInterface {
+    boolean getIsMoveable();
+    void setIsMoveable(boolean isMoveable);
+    BufferedImage getImage();
+    void setImage(BufferedImage image);
+    boolean getIsRotateable();
+    void setIsRotateable(boolean isRotateable);
+    void placeTile();
+    void removeTile();
+
+}
