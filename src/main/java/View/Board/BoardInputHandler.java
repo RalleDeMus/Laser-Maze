039e7025ef944e7e14264fbb5ac37f33892813c7
s204427 <s204427@student.dtu.dk>
2024-04-18 15:15:10 +0200

    Split Boardpage into modules 2

---
+++ src/main/java/View/Board/BoardInputHandler.java
@@ -0,0 +1,93 @@
+package View.Board;
+
+import Model.Logic.Board;
+import Model.Logic.PointStringPair;
+import Model.Tiles.*;
+import Model.Logic.Board;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class BoardInputHandler extends MouseAdapter implements KeyListener {
+    protected Board board;
+    protected JPanel panel;
+
+    protected int yOffset;
+
+    public BoardInputHandler(Board board, JPanel panel,int yOffset) {
+        this.board = board;
+        this.panel = panel;
+        this.yOffset = yOffset;
+        setupListeners();
+    }
+
+    private void setupListeners() {
+        panel.setFocusable(true);
+        panel.addMouseListener(this);
+        panel.addMouseMotionListener(new MouseMotionAdapter() {
+            @Override
+            public void mouseMoved(MouseEvent e) {
+                board.setCursorPos(e.getX(), e.getY()- yOffset);
+                panel.repaint();
+            }
+        });
+        panel.addKeyListener(this);
+        panel.addComponentListener(new ComponentAdapter() {
+            public void componentShown(ComponentEvent e) {
+                panel.requestFocusInWindow();
+            }
+        });
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+        if (e.getButton() == MouseEvent.BUTTON1) {
+            try {
+                board.addTile(board.getSelectedTile().clone());
+            } catch (CloneNotSupportedException ex) {
+                ex.printStackTrace();
+            }
+        } else if (e.getButton() == MouseEvent.BUTTON3) {
+            board.removeTile();
+        }
+        panel.repaint();
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        System.out.println("Key pressed: " + KeyEvent.getKeyText(e.getKeyCode()));
+        switch (e.getKeyCode()) {
+            case KeyEvent.VK_R:
+                System.out.println("Rotating tile...");
+                board.rotateSelectedTile();
+                break;
+            case KeyEvent.VK_ENTER:
+                // Additional logic if needed
+                break;
+            case KeyEvent.VK_1:
+                board.setSelectedTile(new LaserTile(true, true));
+                break;
+            case KeyEvent.VK_2:
+                board.setSelectedTile(new MirrorTile(true, true));
+                break;
+            case KeyEvent.VK_3:
+                board.setSelectedTile(new DoubleTile(true, true));
+                break;
+            case KeyEvent.VK_4:
+                board.setSelectedTile(new SplitterTile(true, true));
+                break;
+            case KeyEvent.VK_5:
+                board.setSelectedTile(new CheckPointTile(false, true));
+                break;
+            case KeyEvent.VK_6:
+                board.setSelectedTile(new CellBlockerTile());
+                break;
+        }
+        panel.repaint();
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) { }
+
+    @Override
+    public void keyTyped(KeyEvent e) { }
+}
---
+++ src/main/java/View/Board/LaserInputHandler.java
@@ -0,0 +1,23 @@
+package View.Board;
+
+import java.awt.event.KeyEvent;
+
+import Model.Logic.Board;
+import javax.swing.*;
+
+
+public class LaserInputHandler extends BoardInputHandler {
+
+    public LaserInputHandler(Board board, JPanel panel,int yOffset){
+        super(board, panel, yOffset);
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+        super.keyPressed(e); // Call super to handle common key events
+        if (e.getKeyCode() == KeyEvent.VK_L) {
+            board.fireLaser();
+            panel.repaint();
+        }
+    }
+}
