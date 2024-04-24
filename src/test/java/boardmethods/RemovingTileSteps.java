59c5f10d0c7be55c5e93e35bd7ff30099cbee190
s204427 <s204427@student.dtu.dk>
2024-04-24 15:35:06 +0200

    Refactored constructlaser. Fixed test to work with non static board

--- src/test/java/boardmethods/RemovingTileSteps.java
+++ src/test/java/boardmethods/RemovingTileSteps.java
@@ -13,27 +13,28 @@ import static org.junit.Assert.assertNull;
 
 public class RemovingTileSteps {
 
-    @Given("a board with a size of {int} and a square size of {int}")
+    Board board;
+    @Given("a board with a size of {int} and a square size of {int} at level 0")
     public void aBoardWithASizeOfXAndASquareSizeOf(int boardSize, int squareSize) {
 
-        Board.getInstance();
+        board = new Board(boardSize, squareSize, "0");
     }
 
     @And("a MirrorTile at position \\({double})")
     public void aMirrorTileAtPosition(int x, int y) {
-        Board.setCursorPos(2,2);
-        Board.addTile(new MirrorTile(true,true));
+        board.setCursorPos(2,2);
+        board.addTile(new MirrorTile(true,true),true);
     }
 
     @When("the user removes the MirrorTile at \\({double})")
     public void theUserRemovesTheMirrorTileAt(int x, int y) {
-        Board.setCursorPos(2,2);
-        Board.removeTile();
+        board.setCursorPos(2,2);
+        board.removeTile();
     }
 
     @Then("the board should not have a tile at position \\({double})")
     public void theBoardShouldNotHaveATileAtPosition(int x, int y) {
-        Tile removedTile = Board.tiles[2][2];
+        Tile removedTile = board.tiles[2][2];
         assertNull("Tile should be removed from the board", removedTile);
     }
 }
