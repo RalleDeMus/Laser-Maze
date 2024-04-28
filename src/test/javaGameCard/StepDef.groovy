b72c1c84a922fa8ea97f22168f8a736518e39d4e
CasperBB-P <s224515@dtu.dk>
2024-04-28 14:38:43 +0200

    redundant

--- src/test/javaGameCard/StepDef.groovy
+++
@@ -1,9 +0,0 @@
-import io.cucumber.groovy.PendingException
-
-import static io.cucumber.groovy.Hooks.*
-import static io.cucumber.groovy.EN.*
-
-Given(~/^I have initialized a game card with multiple "([^"]*)"$/) { String arg1 ->
-    // Write code here that turns the phrase above into concrete actions
-    throw new PendingException()
-}
